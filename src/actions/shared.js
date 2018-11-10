59369d4e04c5c997ee107067d40cf70915898d42
Giorgi Lomidze <gio.lomidze@gmail.com>
2018-11-10 17:47:10 -0500

    fix: fix typo

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,4 +1,4 @@
-import { getInitalData } from '../utils/api';
+import { getInitialData } from '../utils/api';
 import { receiveUsers } from '../actions/users';
 import { receiveTweets } from '../actions/tweets';
 import { setAuthedUser } from '../actions/authedUser';
@@ -7,7 +7,7 @@ export const AUTHED_ID = 'tylermcginnis';
 
 export function handleInitialData() {
   return dispatch => {
-    return getInitalData().then(({ users, tweets }) => {
+    return getInitialData().then(({ users, tweets }) => {
       dispatch(receiveUsers(users));
       dispatch(receiveTweets(tweets));
       dispatch(setAuthedUser(AUTHED_ID));
